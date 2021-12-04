b1de711d3ada860d5f72e8b6488b018a2c89660e
Dayana20 <39806222+Dayana20@users.noreply.github.com>
2021-12-04 03:23:05 -0500

    added animations and updated

--- animations.js
+++ animations.js
@@ -1,11 +1,21 @@
 var input = '';
+var gua = 0;
+var cue = 0;
+var qui = 0;
+var san = 0;
 
 console.log(this.input);
-
+var chosen = "False";
 function getCharacter0(someForm) {
     input=document.getElementById('image1').value;
     console.log(this.input);
-
+    if (chosen=="True"){
+        // document.getElementById('image1').style.backgroundColor="#FFDDD2";
+        document.getElementById('image2').style.backgroundColor="transparent";
+        document.getElementById('image3').style.backgroundColor="transparent";
+    }
+    document.getElementById('image1').style.backgroundColor="#FFDDD2";
+    chosen="True";
     // https://stackoverflow.com/questions/19211768/saving-a-javascript-variable-for-later-usage
     // save data value
     localStorage.setItem("name", this.input);
@@ -15,7 +25,13 @@ function getCharacter0(someForm) {
 function getCharacter1(someForm) {
     input=document.getElementById('image2').value;
     console.log(this.input);
-
+    if (chosen=="True"){
+        // document.getElementById('image2').style.backgroundColor="#FFDDD2";
+        document.getElementById('image1').style.backgroundColor="transparent";
+        document.getElementById('image3').style.backgroundColor="transparent";
+    }
+    document.getElementById('image2').style.backgroundColor="#FFDDD2";
+    chosen="True";
     // save data value
     localStorage.setItem("name", this.input);
     // alert(input);
@@ -24,7 +40,13 @@ function getCharacter1(someForm) {
 function getCharacter2(someForm) {
     input=document.getElementById('image3').value;
     console.log(this.input);
-
+    if (chosen=="True"){
+        // document.getElementById('image3').style.backgroundColor="#FFDDD2";
+        document.getElementById('image2').style.backgroundColor="transparent";
+        document.getElementById('image1').style.backgroundColor="transparent";
+    }
+    document.getElementById('image3').style.backgroundColor="#FFDDD2";
+    chosen="True";
     // save data value
     localStorage.setItem("name", this.input);
     // alert(input);
@@ -44,21 +66,50 @@ if(elem){
     document.getElementById("img4").src =this.name;
 }
 
+var elem2 = document.getElementById('parallax_end');
+if(elem2){
+    console.log("setting up2");
+    document.getElementById("img_end").src =this.name;
+}
+
 console.log("finished switching images");
 
 function changeText(id) {
+    // Guayaquil
     if(id=="food"){
         document.getElementById("message").innerHTML = "This is Encebollado! Traditional Guayaquileño Food";
+        gua = 1;
     }
     else if(id=="food1"){
         document.getElementById("message").innerHTML = "Anything With Green Plantain Is Traditional Food";
+        gua = 1;
     }
     else if(id=="food2"){
         document.getElementById("message").innerHTML = "New Years? Lets Burn Things";
+        gua = 1;
     }
-    else{ // if(id=="food3"){
+    else if(id=="food3"){
         document.getElementById("message").innerHTML = "Don't Go Outside If You Don't Want To End Up In A Strangers Pool";
+        gua = 1;
     }
+    // Quito
+    else if(id=="elem0"){
+        document.getElementById("messageQ").innerHTML = "This is Encebollado! Traditional Guayaquileño Food";
+        qui = 1;
+    }
+    else if(id=="elem1"){
+        document.getElementById("messageQ").innerHTML = "Anything With Green Plantain Is Traditional Food";
+        qui = 1;
+    }
+    else if(id=="elem2"){
+        document.getElementById("messageQ").innerHTML = "New Years? Lets Burn Things";
+        qui = 1;
+    }
+    else if(id=="elem3"){
+        document.getElementById("messageQ").innerHTML = "Don't Go Outside If You Don't Want To End Up In A Strangers Pool";
+        qui = 1;
+    }
+
 
     // alert(id);
 }
@@ -84,192 +135,11 @@ function showImg() {
 }
 
 
+function completion(){
+    if(gua==1 && qui==1){
+        document.getElementById("theEnd").style.display = "block";
+        document.getElementById("keepGoing").style.display = "none";
 
-
-
-
-
-
-// try this
-const button = document.querySelector('.js-toggle-button');
-const menu = document.querySelector('.js-menu');
-const links = document.querySelectorAll('.js-link');
-
-const showTextTransitioner = transitionHiddenElement({
-  element: document.querySelector('.js-show-text'),
-  visibleClass: 'is-shown',
-});
-
-const hideTextTransitioner = transitionHiddenElement({
-  element: document.querySelector('.js-hide-text'),
-  visibleClass: 'is-shown',
-})
-
-const transitionDelay = 0.025;
-const transitionLength = 0.2;
-const itemLength = 6;
-const staggeredTransitionLength = (transitionDelay * (itemLength - 1)) + transitionLength;
-
-const menuTransitioner = transitionHiddenElement({
-  element: document.querySelector('.js-menu'),
-  visibleClass: 'is-open',
-  waitMode: 'timeout',
-  timeoutDuration: staggeredTransitionLength * 1000
-})
-
-button.addEventListener('click', () => {
-  toggleMenu();
-});
-
-links.forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    toggleMenu();
-  });
-});
-
-const toggleMenu = () => {
-  showTextTransitioner.toggle();
-  hideTextTransitioner.toggle();
-  menuTransitioner.toggle();
-}
-
-/** 
- * Library code
- * Using https://www.npmjs.com/package/@cloudfour/transition-hidden-element
- */
-
-function transitionHiddenElement({
-  element,
-  visibleClass,
-  waitMode = 'transitionend',
-  timeoutDuration,
-  hideMode = 'hidden',
-  displayValue = 'block'
-}) {
-  if (waitMode === 'timeout' && typeof timeoutDuration !== 'number') {
-    console.error(`
-      When calling transitionHiddenElement with waitMode set to timeout,
-      you must pass in a number for timeoutDuration.
-    `);
-
-    return;
-  }
-
-  // Don't wait for exit transitions if a user prefers reduced motion.
-  // Ideally transitions will be disabled in CSS, which means we should not wait
-  // before adding `hidden`.
-  if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-    waitMode = 'immediate';
-  }
-
-  /**
-   * An event listener to add `hidden` after our animations complete.
-   * This listener will remove itself after completing.
-   */
-  const listener = e => {
-    // Confirm `transitionend` was called on  our `element` and didn't bubble
-    // up from a child element.
-    if (e.target === element) {
-      applyHiddenAttributes();
-
-      element.removeEventListener('transitionend', listener);
     }
-  };
-
-  const applyHiddenAttributes = () => {
-    if(hideMode === 'display') {
-      element.style.display = 'none';
-    } else {
-      element.setAttribute('hidden', true);
-    }
-  }
-
-  const removeHiddenAttributes = () => {
-    if(hideMode === 'display') {
-      element.style.display = displayValue;
-    } else {
-      element.removeAttribute('hidden');
-    }
-  }
-
-  return {
-    /**
-     * Show the element
-     */
-    show() {
-      /**
-       * This listener shouldn't be here but if someone spams the toggle
-       * over and over really fast it can incorrectly stick around.
-       * We remove it just to be safe.
-       */
-      element.removeEventListener('transitionend', listener);
-
-      /**
-       * Similarly, we'll clear the timeout in case it's still hanging around.
-       */
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-      }
-
-      removeHiddenAttributes();
-
-      /**
-       * Force a browser re-paint so the browser will realize the
-       * element is no longer `hidden` and allow transitions.
-       */
-      const reflow = element.offsetHeight;
-
-      element.classList.add(visibleClass);
-    },
-
-    /**
-     * Hide the element
-     */
-    hide() {
-      if (waitMode === 'transitionend') {
-        element.addEventListener('transitionend', listener);
-      } else if (waitMode === 'timeout') {
-        this.timeout = setTimeout(() => {
-          applyHiddenAttributes();
-        }, timeoutDuration);
-      } else {
-        applyHiddenAttributes();
-      }
-
-      // Add this class to trigger our animation
-      element.classList.remove(visibleClass);
-    },
-
-    /**
-     * Toggle the element's visibility
-     */
-    toggle() {
-      if (this.isHidden()) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    },
-
-    /**
-     * Tell whether the element is hidden or not.
-     */
-    isHidden() {
-      /**
-       * The hidden attribute does not require a value. Since an empty string is
-       * falsy, but shows the presence of an attribute we compare to `null`
-       */
-      const hasHiddenAttribute = element.getAttribute('hidden') !== null;
-
-      const isDisplayNone = element.style.display === 'none';
-
-      const hasVisibleClass = [...element.classList].includes(visibleClass);
-
-      return hasHiddenAttribute || isDisplayNone || !hasVisibleClass;
-    },
-
-    // A placeholder for our `timeout`
-    timeout: null
-  };
+    alert(gua,cue,qui,san);
 }
